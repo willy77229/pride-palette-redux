14b3911397d8ef8fecc9b9a652e3246a8d017adc
Willy C <willyc@Willys-MBP.hsd1.ca.comcast.net>
2023-08-31 15:37:54 -0700

    added stripe payment serverless function

--- src/components/button/button.styles.jsx
+++ src/components/button/button.styles.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { SpinnerContainer } from "../spinner/spinner.styles";
 
 export const BaseButton = styled.button`
     min-width: 165px; 
@@ -16,7 +17,8 @@ export const BaseButton = styled.button`
     border: none; 
     cursor: pointer; 
     display: flex; 
-    justify-content: center; 
+    justify-content: center;
+    align-items: center;
     
     &:hover { 
         background-color: white; 
@@ -47,3 +49,7 @@ export const InvertedButton = styled(BaseButton)`
     } 
 `;
 
+export const ButtonSpinner = styled(SpinnerContainer)`
+    width: 30px;
+    height: 30px;
+`
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { selectCartItems, selectCartTotal } from "../../store/cart/cart.selector";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 import { CheckoutContainer, CheckoutHeader, HeaderBlock, Total } from "./checkout.styles";
+import PaymentForm from "../../components/payment-form/payment-form.component";
 
 const Checkout = () => {
     const cartItems = useSelector(selectCartItems);
@@ -30,6 +31,7 @@ const Checkout = () => {
                     <CheckoutItem key={cartItem.id} cartItem={cartItem} />
                 ))}
                 <Total>Total: ${cartTotal}</Total>
+                <PaymentForm />
         </CheckoutContainer>
     )
 }
